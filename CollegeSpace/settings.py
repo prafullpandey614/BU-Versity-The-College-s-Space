2a7f33e4553c22c56709d0dd2f3482cdd3e0c371
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-12-04 14:26:21 +0530

    ak 1st

--- CollegeSpace/settings.py
+++ CollegeSpace/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = BASE_DIR, '/static'
+
+STATIC_ROOT=BASE_DIR /'static'
