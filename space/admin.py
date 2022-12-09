a652090014f54dd43f32f42518377a176377db15
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-12-09 15:40:05 +0530

    questionpaper1

--- space/admin.py
+++ space/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Branch,Paper,Semester,Year
 # Register your models here.
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display= ['user', 'sem']
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display= ['user', 'branch']
+
+@admin.register(Year)
+class BranchAdmin(admin.ModelAdmin):
+    list_display= ['user', 'year']
+
+@admin.register(Paper)
+class PaperAdmin(admin.ModelAdmin):
+    list_display= ['branch', 'sem', 'year', 'paper_name', 'paper_pdf']
