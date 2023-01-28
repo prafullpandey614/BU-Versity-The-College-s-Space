ee49c778609962e321bfc48697382b26074153b6
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-01-28 23:41:31 +0530

    Update settings.py

--- CollegeSpace/settings.py
+++ CollegeSpace/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
